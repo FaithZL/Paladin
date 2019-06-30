c7038aa85aa6f7d7ac50f0c054889a607b548e4d
SATAN_Z <249002031@qq.com>
2019-06-30 10:37:58 +0800

    Initial Commit

---
+++ Paladin/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Paladin
+//
+//  Created by SATAN_Z on 2019/6/30.
+//  Copyright © 2019 Zero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
