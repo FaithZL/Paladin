25685405375a518dbc5e05ff7fe167fd3823e630
FaithZL <249002031@qq.com>
2019-07-24 22:15:29 +0800

    实现采样方法

--- Paladin/src/core/shape.cpp
+++ Paladin/src/core/shape.cpp
@@ -23,6 +23,27 @@ Bounds3f Shape::worldBound() const {
 	return _objectToWorld->exec(objectBound());
 }
 
+Interaction Shape::sample(const Interaction &ref, const Point2f &u, Float *pdf) const {
+	Interaction intr = sample(u, pdf);
+	Vector3f wi = intr.pos - ref.pos;
+	if (wi.lengthSquared() == 0) {
+		*pdf = 0.f;
+	} else {
+		wi = normalize(wi);
+		*pdf *= distanceSquared(ref.pos, intr.pos) / absDot(intr.normal, -wi);
+		if (std::isinf(*pdf)) {
+			*pdf = 0.f;
+		}
+	}
+	return intr;
+}
 
+
+Float Shape::pdf(const Interaction &ref, const Vector3f &wi) const {
+	Ray ray = ref.spawnRay(wi);
+	Float tHit;
+	SurfaceInteraction isectLight;
+	
+}
 
 PALADIN_END
