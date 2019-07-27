c87993834ef15d7fc43a39318dc0440f3b181e62
SATAN_Z <249002031@qq.com>
2019-07-28 01:09:34 +0800

    实现采样函数

--- Paladin/src/core/sampling.cpp
+++ Paladin/src/core/sampling.cpp
@@ -7,3 +7,33 @@
 //
 
 #include "sampling.hpp"
+
+PALADIN_BEGIN
+
+Vector3f uniformSampleHemisphere(const Point2f &u) {
+    Float z = u[0];
+    Float r = std::sqrt(std::max((Float)0, (Float)1. - z * z));
+    Float phi = 2 * Pi * u[1];
+    return Vector3f(r * std::cos(phi), r * std::sin(phi), z);
+}
+
+Vector3f uniformSampleSphere(const Point2f &u) {
+    Float z = 1 - 2 * u[0];
+    Float r = std::sqrt(std::max((Float)0, (Float)1 - z * z));
+    Float phi = 2 * Pi * u[1];
+    return Vector3f(r * std::cos(phi), r * std::sin(phi), z);
+}
+
+Float uniformConePdf(Float cosThetaMax) {
+    return 1 / (2 * Pi * (1 - cosThetaMax));
+}
+
+Vector3f uniformSampleCone(const Point2f &u, Float cosThetaMax) {
+    Float cosTheta = ((Float)1 - u[0]) + u[0] * cosThetaMax;
+    Float sinTheta = std::sqrt((Float)1 - cosTheta * cosTheta);
+    Float phi = u[1] * 2 * Pi;
+    return Vector3f(std::cos(phi) * sinTheta, std::sin(phi) * sinTheta,
+                    cosTheta);
+}
+
+PALADIN_END
--- Paladin/src/core/sampling.hpp
+++ Paladin/src/core/sampling.hpp
@@ -27,8 +27,107 @@ p(θ, φ)dθdφ = p(w)dw   3式
 
 p(θ, φ) = sinθ p(w)  4式
 
-如果X,Y两个随机变量相互独立
+如果X,Y两个随机变量相互独立，则
 p(x, y) = px(x) * py(y)  5式
+
+p(x,y)为2维随机变量的联合概率密度函数
+则边缘概率密度函数，
+p(x) = ∫p(x,y)dy    6式
+
+条件概率密度函数
+p(y|x) = p(x,y) / p(x)  7式
+
+要生成指定分布的随机数，逆变换算法很常用
+	积分求出指定pdf对应的cdf，记为F(x)
+	y = F(x)定义域为[-∞,+∞],值域为[0,1]
+	求出F(x)反函数G(x)
+	G(x)定义域域为[0,1],值域为[-∞,+∞]
+	在G(x)的定义域上均匀采样，得到的G(x)值就服从指定的pdf分布
+	注释不方便画图，原理的话可以自行画图体验
+
  */
+PALADIN_BEGIN
+
+/*
+ 半球表面上生成均匀的随机点
+ 半球的立体角为2π
+ ∫[0, 2π]p(w)dw = 1
+ 均匀分布p(w)为常数c，求解 c = p(w) = 1 / 2π，又由3式得 p(θ,φ) = sinθ / 2π
+
+ p(θ) = ∫[0,2π]p(θ,φ)dφ = ∫[0,2π](sinθ/2π)dφ = sinθ  
+ p(φ|θ) = p(φ,θ)/p(θ) = 1/2π
+ 只要能按照如上的pdf生成对应的θ与φ，就可以均匀采样半球了
+ 
+ 对p(θ)积分: P(θ) = ∫[0,θ]sinθ'dθ' = 1-cosθ
+ 对p(φ|θ)积分: P(φ|θ) = ∫[0,φ]1/2πdφ' = φ/2π
+ 有了上述两个累积分布函数，可以用逆变换算法求出θ与φ的指定分布
+ a,b为[0,1]的均匀分布随机数
+ θ = arccos(1 - a)，由于是均匀分布随机数1-a可以用a代替
+ θ = arccos(a)
+ φ = 2πb
+ 综合sphere.hpp中球的参数方程
+ x = cos(2πb) * sqrt(1-a^2)
+ y = sin(2πb) * sqrt(1-a^2)
+ z = a
+ */
+Vector3f uniformSampleHemisphere(const Point2f &u);
+
+/*
+ 在一个球表面上生成均匀的随机点
+ 球的立体角为4π
+ ∫[0, 4π]p(w)dw = 1
+ 均匀分布p(w)为常数c，求解 c = p(w) = 1 / 4π，又由3式得 p(θ,φ) = sinθ/4π
+
+ p(θ) = ∫[0,2π]p(θ,φ)dφ = ∫[0,2π](sinθ/4π)dφ = sinθ/2
+ p(φ|θ) = p(φ,θ)/p(θ) = 1/2π
+
+ 对p(θ)积分: P(θ) = ∫[0,θ](1/2)sinθ'dθ' = (1/2)*(1-cosθ)
+ 对p(φ|θ)积分: P(φ|θ) = ∫[0,φ]1/2πdφ' = φ/2π
+ a,b为[0,1]的均匀分布随机数
+ θ = arccos(1-2a)
+ φ = 2πb
+ 综合sphere.hpp中球的参数方程
+ x = cos(2πb) * 2 * sqrt(a-a^2)
+ y = sin(2πb) * 2 * sqrt(a-a^2)
+ z = 1 - 2a 
+ */
+Vector3f uniformSampleSphere(const Point2f &u);
+
+
+/*
+ 均匀采样一个圆锥的pdf函数
+
+ p(θ, φ) = sinθ p(w)
+ p(θ, φ) = pθ(θ) * pφ(φ)
+ pφ(φ) = 1/2π
+
+ p(w) = pθ(θ)/(2π * sinθ) 为常数
+ 所以p(θ)/sinθ为常数
+ 假设p(θ) = c * sinθ
+
+ 1 = ∫[0,θmax]p(θ)dθ
+ 求得 c = 1/(1 - cosθmax)
+ p(θ) = sinθ/(1 - cosθmax)
+ p(w) = p(θ, φ)/sinθ = pθ(θ) * pφ(φ)/sinθ = 1/(2π(1-cosθmax))
+ */
+Float uniformConePdf(Float cosThetaMax);
+
+/*
+ 均匀采样一个圆锥，默认圆锥的中心轴为(0,0,1)，圆锥顶点为坐标原点
+ p(θ) = sinθ/(1 - cosθmax)
+
+ 积分计算得到累积分布函数
+ P(θ) = (cosθ - 1)/(cosθmax - 1)
+ P(φ) = 1/2π
+ */
+Vector3f uniformSampleCone(const Point2f &u, Float cosThetaMax);
+
+PALADIN_END
+
 
 #endif /* sampling_hpp */
+
+
+
+
+
