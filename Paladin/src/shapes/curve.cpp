a3057037d3b412770d3e100c2ddc65631aa5793f
SATAN_Z <249002031@qq.com>
2019-08-01 00:05:47 +0800

    添加curve文件

---
+++ Paladin/src/shapes/curve.cpp
@@ -0,0 +1,9 @@
+//
+//  curve.cpp
+//  Paladin
+//
+//  Created by SATAN_Z on 2019/8/1.
+//  Copyright © 2019 Zero. All rights reserved.
+//
+
+#include "curve.hpp"
---
+++ Paladin/src/shapes/curve.hpp
@@ -0,0 +1,14 @@
+//
+//  curve.hpp
+//  Paladin
+//
+//  Created by SATAN_Z on 2019/8/1.
+//  Copyright © 2019 Zero. All rights reserved.
+//
+
+#ifndef curve_hpp
+#define curve_hpp
+
+#include <stdio.h>
+
+#endif /* curve_hpp */
