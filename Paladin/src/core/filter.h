f356ec99af35e4461bb17000df3507b233fc7303
SATAN_Z <249002031@qq.com>
2019-08-20 23:44:49 +0800

    保存进度

---
+++ Paladin/src/core/filter.h
@@ -0,0 +1,36 @@
+//
+//  filter.h
+//  Paladin
+//
+//  Created by SATAN_Z on 2019/8/20.
+//
+
+#ifndef filter_h
+#define filter_h
+
+#include "header.h"
+
+PALADIN_BEGIN
+
+class Filter {
+    
+public:
+    virtual ~Filter() {
+        
+    }
+    
+    Filter(const Vector2f &radius)
+    : radius(radius),
+    invRadius(Vector2f(1 / radius.x, 1 / radius.y)) {
+        
+    }
+    
+    virtual Float evaluate(const Point2f &p) const = 0;
+    
+    const Vector2f radius;
+    const Vector2f invRadius;
+};
+
+PALADIN_END
+
+#endif /* filter_h */
