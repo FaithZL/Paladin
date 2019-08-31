56298a3070592472149c02e6dd21569c5d6dbb77
SATAN_Z <249002031@qq.com>
2019-08-31 13:39:41 +0800

    修改注释

--- Paladin/src/core/sampler.cpp
+++ Paladin/src/core/sampler.cpp
@@ -7,3 +7,21 @@
 //
 
 #include "sampler.hpp"
+
+
+PALADIN_BEGIN
+
+Sampler::Sampler(int64_t samplesPerPixel)
+: samplesPerPixel(samplesPerPixel) {
+    
+}
+
+void Sampler::startPixel(const Point2i &p) {
+    _currentPixel = p;
+    _currentPixelSampleIndex = 0;
+    _array1DOffset = _array2DOffset = 0;
+}
+
+
+
+PALADIN_END
