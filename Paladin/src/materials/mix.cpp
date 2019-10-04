92452b9c5b55ffcb6608a2fa174a7c8f84e482d5
SATAN_Z <249002031@qq.com>
2019-10-05 00:44:13 +0800

    实现mix材质

--- Paladin/src/materials/mix.cpp
+++ Paladin/src/materials/mix.cpp
@@ -6,3 +6,29 @@
 //
 
 #include "mix.hpp"
+#include "core/bxdf.hpp"
+#include "core/texture.hpp"
+PALADIN_BEGIN
+
+void MixMaterial::computeScatteringFunctions(SurfaceInteraction *si,
+                                             MemoryArena &arena,
+                                             TransportMode mode,
+                                             bool allowMultipleLobes) const {
+    Spectrum s1 = _scale->evaluate(*si).Clamp();
+    Spectrum s2 = (Spectrum(1.f) - s1).Clamp();
+    SurfaceInteraction si2 = *si;
+    _m1->computeScatteringFunctions(si, arena, mode, allowMultipleLobes);
+    _m2->computeScatteringFunctions(&si2, arena, mode, allowMultipleLobes);
+    
+    int n1 = si->bsdf->numComponents();
+    int n2 = si2.bsdf->numComponents();
+    for (int i = 0; i < n1; ++i) {
+        // 注意，这里是需要对bxdf数组，直接赋值，覆盖之前的bxdf
+        si->bsdf->bxdfs[i] = ARENA_ALLOC(arena, ScaledBxDF)(si->bsdf->bxdfs[i], s1);
+    }
+    for (int i = 0; i < n2; ++i) {
+        si->bsdf->add(ARENA_ALLOC(arena, ScaledBxDF)(si2.bsdf->bxdfs[i], s2));
+    }
+}
+
+PALADIN_END
--- Paladin/src/materials/mix.hpp
+++ Paladin/src/materials/mix.hpp
@@ -8,6 +8,31 @@
 #ifndef material_mix_hpp
 #define material_mix_hpp
 
-#include <stdio.h>
+#include "core/header.h"
+#include "core/material.hpp"
+
+PALADIN_BEGIN
+
+class MixMaterial : public Material {
+    
+public:
+    
+    MixMaterial(const std::shared_ptr<Material> &m1,
+                const std::shared_ptr<Material> &m2,
+                const std::shared_ptr<Texture<Spectrum>> &scale)
+    : _m1(m1), _m2(m2), _scale(scale) {
+        
+    }
+    
+    virtual void computeScatteringFunctions(SurfaceInteraction *si, MemoryArena &arena,
+                                            TransportMode mode,
+                                            bool allowMultipleLobes) const;
+    
+private:
+    std::shared_ptr<Material> _m1, _m2;
+    std::shared_ptr<Texture<Spectrum>> _scale;
+};
+
+PALADIN_END
 
 #endif /* material_mix_hpp */
