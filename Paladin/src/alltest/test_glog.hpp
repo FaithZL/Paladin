e303d3160080db6e5388228edb8b790fb8a53b49
huangshengshuai <huangshengshuai@aobi.com>
2019-11-04 10:11:48 +0800

    添加glog库

---
+++ Paladin/src/alltest/test_glog.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <glog/logging.h>
+
+int test_glog_main(const char* argv) {
+    FLAGS_alsologtostderr = 1;
+    google::InitGoogleLogging(argv);
+
+    LOG(INFO) << "I am INFO!";
+    LOG(WARNING) << "I am WARNING!";
+    LOG(ERROR) << "I am ERROR!";
+    // LOG(FATAL) << "I am FATAL!";
+
+    return 0;
+}
