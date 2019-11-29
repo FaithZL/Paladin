65a271dcae59587579f9c6bb1b48786bbd71d37e
Zero <249002031@qq.com>
2019-11-30 00:28:03 +0800

    配置场景，添加gallery

--- Paladin/src/main.cpp
+++ Paladin/src/main.cpp
@@ -25,56 +25,13 @@ USING_PALADIN
 
 USING_STD
 
-class A {
-    
-public:
-    int m = 0;
-};
-
-void f2(const A * a) {
-//    a->m = 2;
-    cout <<a->m;
-}
-
-class Test {
-public:
-    Test() {
-        p = new A();
-        p->m = 9;
-    }
-    
-    void fun() const {
-        f2(p);
-    }
-    
-public:
-    A * p;
-};
-
 int main(int argc, const char * argv[]) {
     // insert code here...
     COUT << "Hello, paladin!\n";
     
-//    Test t;
-//
-//    cout << t.p->m << endl;
-//    t.fun();
-//    cout << t.p->m << endl;
-    
     Paladin paladin;
-    paladin.render("res/simplescene.json");
-//    unique_ptr<int> a;
-//    a.reset()
-//    paladin.render("res/scene.json");
-    
-//    test_lua_main();
-//    test_rapidxml_main();
-//    loadfile();
-//    testscene();
-//    test_glog_main(argv[0]);
-//    testscene();
-//    testjson();
-//    testCache();
+    string fileName(argv[1]);
+    paladin.render(fileName);
 #ifdef _MSC_VER
     //让VS运行debug时不至于黑屏一闪而过
     system("pause");
