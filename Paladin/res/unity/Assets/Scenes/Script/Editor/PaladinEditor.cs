1dd95d77bd308e3ea86c50b6216d8edb42cd6a69
Zero <249002031@qq.com>
2020-02-27 21:08:57 +0800

    添加unity资源
    unity脚本，用于将unity场景导出为paladin能解析的json格式

---
+++ Paladin/res/unity/Assets/Scenes/Script/Editor/PaladinEditor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(Paladin))]
+public class PaladinEditor : Editor
+{
+
+    Paladin paladin;
+
+    void OnEnable()
+    {
+        //获取当前编辑自定义Inspector的对象
+        paladin = (Paladin)target;
+        Debug.Log("OnEnable");
+    }
+
+    void UpdateSamplerParam()
+    {
+
+    }
+
+    void UpdateFilterParam()
+    {
+
+    }
+
+    //public override void OnInspectorGUI()
+    //{
+    //    EditorGUILayout.BeginVertical();
+
+        
+    //}
+}
---
+++ Paladin/res/unity/Assets/Scenes/Script/Paladin.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum Filter
+{
+    box,
+    triange,
+    gaussian,
+    mitchell,
+    sinc
+};
+
+public enum Sampler
+{
+    halton,
+    random,
+    stratified
+}
+
+public enum FileFormat { png,exr }
+
+public class Paladin : MonoBehaviour
+{
+
+    [Header("filter param")]
+    public Filter filterName;
+    [Range(0, 1)]
+    public float filterRadius = 1.0f;
+
+    [Header("film param")]
+    public Vector2Int res = new Vector2Int(100,100);
+    public Rect cropWindow = new Rect(0,0,1,1);
+    public string outputName = "paladin";
+    public FileFormat fileFormat;
+    [Range(0, 1)]
+    public float scale = 1.0f;
+    [Range(0, 1)]
+    public float diagonal = 1.0f;
+
+    [Header("sampler")]
+    public Sampler samplerName;
+
+
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Export()
+    {
+        Camera camera = GameObject.FindObjectOfType<Camera>() as Camera;
+        
+        MeshFilter[] primitives = GameObject.FindObjectsOfType<MeshFilter>() as MeshFilter[];
+        var shape = primitives[0];
+        Material material = shape.GetComponent<Renderer>().sharedMaterial;
+
+        var albedo = material.GetColor("Albedo");
+
+        Light[] lights = GameObject.FindObjectsOfType<Light>() as Light[];
+    }
+}
