8d413daa3c3df4e120393c94ae7e7f8182ab8a6f
Zero <249002031@qq.com>
2020-03-06 22:56:16 +0800

    unity场景数据导出保存进度
    还差材质数据的导出，还需要查查如何搞

--- Paladin/res/unity/Assets/Scenes/Script/Enum.cs
+++ Paladin/res/unity/Assets/Scenes/Script/Enum.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public enum Filter {
     box,
-    triange,
+    triangle,
     gaussian,
     mitchell,
     sinc
@@ -41,5 +41,67 @@ public enum AclrtSplitMethod {
 
 public enum FileFormat {
     png,
-    exr
+    exr,
+    jpg
+}
+
+class PaladinEnum {
+
+    public string toString() {
+        return "PaladinEnum";
+    }
+
+    public static string getName(FileFormat type) {
+        switch (type) {
+            case FileFormat.exr:
+                return "exr";
+            case FileFormat.png:
+                return "png";
+            case FileFormat.jpg:
+                return "jpg";
+            default:
+                return "";
+        }
+    }
+
+    public static string getName(LightSampleStrategy type) {
+        switch (type) {
+            case LightSampleStrategy.power:
+                return "power";
+            case LightSampleStrategy.uniform:
+                return "uniform";
+            default:
+                return "";
+        }
+    }
+
+    public static string getName(Filter type) {
+        switch (type) {
+            case Filter.box:
+                return "box";
+            case Filter.triangle:
+                return "triangle";
+            case Filter.sinc:
+                return "sinc";
+            case Filter.mitchell:
+                return "mitchell";
+            case Filter.gaussian:
+                return "gaussian";
+            default:
+                return "";
+        }
+    }
+
+    public static string getName(AclrtSplitMethod type) {
+        switch (type) {
+            case AclrtSplitMethod.EqualCounts:
+                return "EqualCounts";
+            case AclrtSplitMethod.Middle:
+                return "Middle";
+            case AclrtSplitMethod.SAH:
+                return "SAH";
+            default:
+                return "";
+        }
+    }
 }
--- Paladin/res/unity/Assets/Scenes/Script/Paladin.cs
+++ Paladin/res/unity/Assets/Scenes/Script/Paladin.cs
@@ -23,7 +23,7 @@ public class Paladin : MonoBehaviour {
 
 
     [Header("--------film param--------")]
-    public Vector2Int res = new Vector2Int(100,100);
+    public Vector2Int resolution = new Vector2Int(500,500);
     public Rect cropWindow = new Rect(0,0,1,1);
     public string outputName = "paladin";
     public FileFormat fileFormat;
@@ -54,7 +54,6 @@ public class Paladin : MonoBehaviour {
     public float shutterClose = 1;
     public float lensRadius = 0;
     public float focalDistance = 100;
-    public float fov = 45;
 
     [Header("--------accelerator--------")]
     public Accelerator acceleratorName;
@@ -91,7 +90,42 @@ public class Paladin : MonoBehaviour {
     }
 
     void handleCamera() {
-        
+
+        _camera = GameObject.FindObjectOfType<Camera>() as Camera;
+
+        var param = new JsonData();
+
+        var pos = _camera.transform.localPosition;
+        var target = _camera.transform.forward + pos;
+        var up = _camera.transform.up;
+
+        var lookAt = new JsonData();
+        lookAt.Add(fromVec3(pos));
+        lookAt.Add(fromVec3(target));
+        lookAt.Add(fromVec3(up));
+
+        param["lookAt"] = lookAt;
+
+        param["shutterOpen"] = shutterOpen;
+        param["shutterClose"] = shutterClose;
+        param["fov"] = _camera.fieldOfView;
+        param["lensRadius"] = lensRadius;
+        param["focalDistance"] = focalDistance;
+
+        var cameraData = new JsonData();
+        cameraData["type"] = "perspective";
+        cameraData["param"] = param;
+
+        _output["camera"] = cameraData;
+
+    }
+
+    JsonData fromVec3(Vector3 v) {
+        var ret = new JsonData();
+        ret.Add((double)v.x);
+        ret.Add((double)v.y);
+        ret.Add((double)v.z);
+        return ret;
     }
 
     void handleThreadNum() {
@@ -104,7 +138,7 @@ public class Paladin : MonoBehaviour {
         var param = new JsonData();
 
         samplerData["param"] = param;
-        
+
         switch (samplerName) {
             case Sampler.halton:
                 samplerData["type"] = "halton";
@@ -135,24 +169,17 @@ public class Paladin : MonoBehaviour {
         radius.Add((double)filterRadius.y);
         param["radius"] = radius;
 
+        filterData["type"] = PaladinEnum.getName(filterName);
+
         switch (filterName) {
-            case Filter.box:
-                filterData["type"] = "box";
-                break;
-            case Filter.triange:
-                filterData["type"] = "triangle";
-                break;
             case Filter.gaussian:
-                filterData["type"] = "gaussian";
                 param["alpha"] = alpha;
                 break;
             case Filter.mitchell:
-                filterData["type"] = "mitchell";
                 param["B"] = B;
                 param["C"] = C;
                 break;
             case Filter.sinc:
-                filterData["type"] = "sinc";
                 param["tau"] = tau;
                 break;
         }
@@ -161,23 +188,242 @@ public class Paladin : MonoBehaviour {
     }
 
     void handleAccelerator() {
+        var aclrtData = new JsonData();
+        var param = new JsonData();
+        aclrtData["param"] = param;
+        switch (acceleratorName) {
+            case Accelerator.bvh:
+                aclrtData["type"] = "bvh";
+                param["maxPrimsInNode"] = maxPrimsInNode;
+                param["splitMethod"] = PaladinEnum.getName(splitMethod);
+                break;
+            case Accelerator.kdTree:
+                //暂时不支持kdtree
+                Assert.IsFalse(false);
+                break;
+        }
 
+        _output["accelerator"] = aclrtData;
     }
 
     void handleFilm() {
+        var filmData = new JsonData();
+        var param = new JsonData();
+        filmData["param"] = param;
 
+        var res = new JsonData();
+        res.Add((double)resolution.x);
+        res.Add((double)resolution.y);
+        param["resolution"] = res;
+
+        var cw = new JsonData();
+        cw.Add((double)cropWindow.x);
+        cw.Add((double)cropWindow.y);
+        cw.Add((double)cropWindow.width);
+        cw.Add((double)cropWindow.height);
+        param["cropWindow"] = cw;
+
+        var fn = outputName + "." + PaladinEnum.getName(fileFormat);
+        param["fileName"] = fn;
+
+        param["scale"] = scale;
+        param["diagonal"] = diagonal;
+
+        _output["film"] = filmData;
     }
 
     void handleIntegrator() {
+        var integratorData = new JsonData();
+        var param = new JsonData();
+        integratorData["param"] = param;
 
+        switch (integratorName) {
+            case Integrator.path:
+                integratorData["type"] = "pt";
+                param["maxBounce"] = maxBounce;
+                param["rrThreshold"] = rrThreshold;
+                param["lightSampleStrategy"] = PaladinEnum.getName(lightSampleStrategy);
+                break;
+            case Integrator.bdpt:
+                integratorData["type"] = "bdpt";
+                param["maxBounce"] = maxBounce;
+                param["rrThreshold"] = rrThreshold;
+                param["lightSampleStrategy"] = PaladinEnum.getName(lightSampleStrategy);
+                break;
+            case Integrator.volpt:
+                integratorData["type"] = "volpt";
+                param["maxBounce"] = maxBounce;
+                param["rrThreshold"] = rrThreshold;
+                param["lightSampleStrategy"] = PaladinEnum.getName(lightSampleStrategy);
+                break;
+            case Integrator.Geometry:
+                integratorData["type"] = "Geometry";
+                param["type"] = "normal";
+                break;
+        }
+        _output["integrator"] = integratorData;
     }
 
     void handleLights() {
+        var lightData = new JsonData();
+        _output["lights"] = lightData;
+        Light[] lights = GameObject.FindObjectsOfType<Light>() as Light[];
 
+        for(int i = 0; i < lights.Length; ++i) {
+            Light light = lights[i];
+            if(light.type == LightType.Directional
+                || light.type == LightType.Point
+                || light.type == LightType.Spot) {
+                lightData.Add(getLight(light));
+            }
+        }
+    }
+
+    JsonData getLight(Light light) {
+        switch (light.type) {
+            case LightType.Point:
+                return getPointLight(light);
+            case LightType.Spot:
+                return getSpotLight(light);
+            case LightType.Directional:
+                return getDistantLight(light);
+            default:
+                return null;
+        }
+    }
+
+    JsonData getPointLight(Light light) {
+        var ret = new JsonData();
+        ret["type"] = "pointLight";
+
+        var param = new JsonData();
+        var pos = light.transform.localPosition;
+        var tf = new JsonData();
+        tf["type"] = "translate";
+        tf["param"] = fromVec3(pos);
+        param["transform"] = tf;
+
+        var I = new JsonData();
+        I["colorType"] = 1;
+        var c = light.color;
+        I["color"] = fromVec3(new Vector3(c.r, c.g, c.b));
+        param["I"] = I;
+
+        param["scale"] = light.intensity;
+
+        ret["param"] = param;
+
+        return ret;
+    }
+
+    JsonData getDistantLight(Light light) {
+        var ret = new JsonData();
+
+        ret["type"] = "distant";
+        var param = new JsonData();
+        var fwd = light.transform.forward;
+        param["wLight"] = fromVec3(-fwd);
+
+        var L = new JsonData();
+        L["colorType"] = 1;
+        var c = light.color;
+        L["color"] = fromVec3(new Vector3(c.r, c.g, c.b));
+        param["L"] = L;
+
+        param["scale"] = light.intensity;
+
+        ret["param"] = param;
+
+        return ret;
+    }
+
+    JsonData getSpotLight(Light light) {
+        var ret = new JsonData(null);
+
+        // todo
+
+        return ret;
     }
 
     void handlePrimitives() {
+        MeshFilter[] primitives = GameObject.FindObjectsOfType<MeshFilter>() as MeshFilter[];
+        var shapeData = new JsonData();
+        for(int i = 0; i < primitives.Length; ++i) {
+            var prim = primitives[i];
+            shapeData.Add(getPrimData(prim));
+        }
+        _output["shapes"] = shapeData;
+    }
 
+    JsonData getPrimData(MeshFilter prim) {
+        var ret = new JsonData();
+
+        ret["type"] = "triMesh";
+        ret["subType"] = "mesh";
+
+        var param = new JsonData();
+        var normals = new JsonData();
+        var verts = new JsonData();
+        var UVs = new JsonData();
+        var material = new JsonData(null);
+        var indexes = new JsonData();
+        var transformData = new JsonData();
+
+        var mesh = prim.sharedMesh;
+
+        for (int i = 0; i < mesh.normals.Length; ++i) {
+            var normal = mesh.normals[i];
+            normals.Add((double)normal.x);
+            normals.Add((double)normal.y);
+            normals.Add((double)normal.z);
+        }
+
+        for(int i = 0; i < mesh.vertices.Length; ++i) {
+            var vert = mesh.vertices[i];
+            verts.Add((double)vert.x);
+            verts.Add((double)vert.y);
+            verts.Add((double)vert.z);
+        }
+
+        for(int i = 0; i < mesh.uv.Length; ++i) {
+            var uv = mesh.uv[i];
+            UVs.Add((double)uv.x);
+            UVs.Add((double)uv.y);
+        }
+
+        for (int i = 0; i < mesh.subMeshCount; ++i) {
+            var indices = mesh.GetIndices(i);
+            for(int j = 0; j < indices.Length; ++j) {
+                indexes.Add(indices[j]);
+            }
+        }
+
+        Matrix4x4 matrix = prim.transform.localToWorldMatrix;
+
+        transformData["type"] = "matrix";
+        var matParam = new JsonData();
+        for(int i = 0; i < 4; ++i) {
+            var row = matrix.GetRow(i);
+            matParam.Add((double)row.x);
+            matParam.Add((double)row.y);
+            matParam.Add((double)row.z);
+            matParam.Add((double)row.w);
+        }
+        transformData["param"] = matParam;
+
+        var mat = prim.GetComponent<Renderer>().sharedMaterial;
+
+        
+
+        ret["param"] = param;
+        param["normals"] = normals;
+        param["verts"] = verts;
+        param["UVs"] = UVs;
+        param["material"] = material;
+        param["indexes"] = indexes;
+        param["transform"] = transformData;
+
+        return ret;
     }
 
     void export() {
