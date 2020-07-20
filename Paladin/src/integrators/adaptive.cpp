64eed96138b412161d7a69057c2e27f6ca7ba779
Zero <249002031@qq.com>
2020-07-20 23:38:47 +0800

    自适应采样保存进度

--- Paladin/src/integrators/adaptive.cpp
+++ Paladin/src/integrators/adaptive.cpp
@@ -6,3 +6,32 @@
 //
 
 #include "adaptive.hpp"
+#include "tools/progressreporter.hpp"
+#include "core/camera.hpp"
+
+PALADIN_BEGIN
+
+
+void AdaptiveIntegrator::render(const Scene &scene) {
+    TRY_PROFILE(Prof::IntegratorRender)
+    _subIntegrator->preprocess(scene, *_sampler);
+    
+    // 由于是并行计算，先把屏幕分割成m * n块
+    AABB2i samplerBounds = _camera->film->getSampleBounds();
+    Vector2i sampleExtent = samplerBounds.diagonal();
+    const int tileSize = 16;
+
+    // 把屏幕x方向分为nTile.x部分，y方向分为nTile.y部分
+    Point2i nTile((sampleExtent.x + tileSize - 1) / tileSize,
+                (sampleExtent.y + tileSize - 1) / tileSize);
+    
+    ProgressReporter reporter("rendering", nTile.x * nTile.y);
+    {
+        auto renderTile = [&](Point2i tile, int threadIdx) {
+            
+        };
+    }
+}
+
+
+PALADIN_END
--- Paladin/src/integrators/adaptive.hpp
+++ Paladin/src/integrators/adaptive.hpp
@@ -12,11 +12,19 @@
 
 PALADIN_BEGIN
 
-class AdaptiveIntegrator : public Integrator {
+//http://luthuli.cs.uiuc.edu/~daf/courses/rendering/Papers-2/RTHWJ.article.pdf
+//https://www.cs.umd.edu/~zwicker/publications/MultidimensionalAdaptiveSampling-SIG08.pdf
+//http://ima.udg.edu/~rigau/publications/rigau03b.pdf
+class AdaptiveIntegrator : public MonteCarloIntegrator {
     
 public:
     
-    AdaptiveIntegrator(Integrator * integrator) {
+    AdaptiveIntegrator(std::shared_ptr<const Camera> camera,
+                       std::shared_ptr<Sampler> sampler,
+                       const AABB2i &pixelBound,
+                       MonteCarloIntegrator * integrator,
+                       Float maxErr)
+    : MonteCarloIntegrator(camera, sampler, pixelBound) {
         _subIntegrator.reset(integrator);
     }
     
@@ -25,17 +33,17 @@ public:
     virtual Spectrum Li(const RayDifferential &ray, const Scene &scene,
                         Sampler &sampler, MemoryArena &arena,
                         int depth = 0) const {
-        
+        return _subIntegrator->Li(ray, scene, sampler, arena, depth);
     }
     
 private:
-    unique_ptr<Integrator> _subIntegrator;
+    unique_ptr<MonteCarloIntegrator> _subIntegrator;
     
     Float _maxError;
     Float _quantile;
     Float _pValue;
     Float _averageLuminance;
-    
+    Float _maxSpp;
 };
 
 PALADIN_END
