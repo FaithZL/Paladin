2ae8f96647e0012cf0d13fb0c8cf1d3f3dca8e50
Zero <249002031@qq.com>
2020-07-21 23:52:21 +0800

    Update adaptive.hpp
    今天学习了置信区间的求法

--- Paladin/src/integrators/adaptive.hpp
+++ Paladin/src/integrators/adaptive.hpp
@@ -15,6 +15,27 @@ PALADIN_BEGIN
 //http://luthuli.cs.uiuc.edu/~daf/courses/rendering/Papers-2/RTHWJ.article.pdf
 //https://www.cs.umd.edu/~zwicker/publications/MultidimensionalAdaptiveSampling-SIG08.pdf
 //http://ima.udg.edu/~rigau/publications/rigau03b.pdf
+
+/*
+ 自适应采样，为了解决一个屏幕内所有像素都用同样的spp数量渲染的问题，
+ 有些像素收敛得比较好，方差比较低，但有些像素方差高，为了使样本产生更多的价值，
+ 我们可以将样本用于收敛比较慢的像素上，方差低到一个既定的阈值时，停止采样，
+ 这就是自适应采样的核心思想
+ 
+ Evaluation of confidence intervals
+ 评估置信区间
+ 
+ P{ L ∈ [L1 : L2] } = 1 - α
+ L1 : L2 为最小最大值范围，可以理解为这是置信区间
+ α为显著性水平，如果α=0.05，那么置信度则是0.95或95%
+ 卧槽，忽然发现这是我没有接触过的知识
+ 
+ 求解置信区间的步骤
+ 第一步：求一个样本的均值
+ 第二步：计算出抽样误差。经过实践，通常认为调查：100个样本的抽样误差为±10%；500个样本的抽样误差为±5%；1200个样本时的抽样误差为±3%。
+ 第三步：用第一步求出的“样本均值”加、减第二步计算的“抽样误差”，得出置信区间的两个端点
+*/
+
 class AdaptiveIntegrator : public MonteCarloIntegrator {
     
 public:
